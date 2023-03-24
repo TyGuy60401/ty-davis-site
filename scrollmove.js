bd9884514bddf5df387166f67bccd7d0e3fcaa74
Ty Davis <tydavisxc2019@gmail.com>
2023-03-24 01:46:52 -0600

    Scrolling is going really well now

--- scrollmove.js
+++ scrollmove.js
@@ -3,8 +3,15 @@ var foreground;
 var mountainStartPos = 20;
 var foregroundStartPos = 20;
 function init() {
-    mountain = document.getElementById("midmountain");
-    foreground = document.getElementById("foreground");
+    if (window.innerWidth < 1000) {
+        var mountainId = "midmountain-mobile";
+        var foregroundId = "foreground-mobile";
+    } else {
+        var mountainId = "midmountain-desktop";
+        var foregroundId = "foreground-desktop";
+    }
+    mountain = document.getElementById(mountainId);
+    foreground = document.getElementById(foregroundId);
 
     mountain.style.top = mountainStartPos + '%';
     foreground.style.top = foregroundStartPos + '%';
@@ -12,13 +19,18 @@ function init() {
 }
 
 function parallax() {
+    var headerText = document.getElementById("overlay");
 
     var yPos = 0 - window.pageYOffset;
     var yPosMountain = yPos/30;
-    var yPosFore = yPos/6;
+    var yPosFore = yPos/7.5;
+    
+    if (yPos > -80) {
+        headerText.style.top = -yPos*7/8 + '%';
+    }
     
     mountain.style.top = mountainStartPos - yPosMountain + '%';
-    if (yPos > -180) {
+    if (yPos > -270) {
         foreground.style.top = foregroundStartPos - yPosFore + '%';
     }
 }
