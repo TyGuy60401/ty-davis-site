981bf891a23f550728962ecc4592fdc1ae22aa8c
Ty Davis <tydavisxc2019@gmail.com>
2023-03-24 00:29:43 -0600

    Working on scrolling behavior

--- scrollmove.js
+++ scrollmove.js
@@ -1,13 +1,32 @@
+var mountain;
+var foreground;
+var mountainStartPos = 20;
+var foregroundStartPos = 200;
+function init() {
+    mountain = document.getElementById("midmountain");
+    foreground = document.getElementById("foreground");
+
+    mountain.style.top = mountainStartPos + '%';
+    foreground.style.top = foregroundStartPos + '%';
+    console.log("Starting...");
+}
+
 function parallax() {
-    var mountain = document.getElementById("midmountain");
-    var foreground = document.getElementById("foreground");
 
     var yPos = 0 - window.pageYOffset;
     var yPosMountain = yPos/30;
     var yPosFore = yPos/4;
     
-    mountain.style.top = 20 - yPosMountain + '%';
-    foreground.style.top = 20 - yPosFore + '%';
+    mountain.style.top = mountainStartPos - yPosMountain + '%';
+    console.log(yPos);
+    if (yPos > -180) {
+        foreground.style.top = foregroundStartPos - yPosFore + '%';
+    } else {
+        console.log("Stop moving");
+    }
+}
+window.onload = function() {
+    init();
 }
 window.addEventListener("scroll", function() {
     parallax();
