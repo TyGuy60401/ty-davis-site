92731127a8de39df65d96f71c4261e78b96c7f29
Ty Davis <tydavisxc2019@gmail.com>
2023-04-13 14:12:46 -0700

    Making progress on the log functionality

--- log/buildLog.js
+++ log/buildLog.js
@@ -1,5 +1,19 @@
 function buildLog(filename) {
     let logMain = document.querySelector('#log-main');
-    $.getJSON("./logFiles/" + filename + ".json");
-    console.log(filename);
+    logMain.innerHTML = "";
+    $.getJSON("./logFiles/" + filename + ".json", function (data) {
+        console.log(data);
+        let title = data['title'];
+        let description = data['description'];
+        let distance = data['distance'];
+        let time = data['time'];
+        let pace = data['pace'];
+        let type = data['type'];
+
+    }).fail( function() {
+        noLogMessage = document.createElement('h3');
+        noLogMessage.innerHTML = "There is no training log file for this date.";
+        logMain.appendChild(noLogMessage);
+        console.log('error');
+    });
 }
