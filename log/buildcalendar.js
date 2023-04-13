c974fbb1f0a0d5d948210db00bb9a741e92b62c2
Ty Davis <tydavisxc2019@gmail.com>
2023-04-12 21:36:06 -0600

    Got the calendar days working

--- log/buildcalendar.js
+++ log/buildcalendar.js
@@ -3,9 +3,30 @@ function buildCalendar(month, year, divID) {
     let parentDiv = document.querySelector('#'+divID);
     parentDiv.innerHTML = calendarTableDiv.innerHTML;
     let calendarTable = parentDiv.firstElementChild;
+    
+    let day = new Date(2023, 2, 12);
+    let currentMonth = day.getMonth();
+    day.setDate(1);
+    dayOfWeek = day.getDay();
+    day.setDate(1-dayOfWeek);
+    let dayNum;
+    let classString;
+
     for (var i=0, row; row = calendarTable.rows[i]; i++) {
         for (var j=0, cell; cell = row.cells[j]; j++) {
-            cell.innerHTML = i*7+j;
+            dayNum = day.getDate();
+
+            if (day.getMonth() == currentMonth) {
+                classString = "'on-month'";
+            } else {
+                classString = "'off-month'";
+            }
+            cell.innerHTML = "<a href='#' class=" + classString + ">" + dayNum + "</a>";
+
+            day.setDate(dayNum + 1);
         }
+    }
+    if (day.getMonth() == currentMonth) {
+        console.log("We need another row");
     }
 }
